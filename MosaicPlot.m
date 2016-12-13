4257489dabc607e90f8db974e862cbe6a490f911
Anton Antonov <antononcube@gmail.com>
2016-12-12 17:31:17 -0800

    Replaced Import with Get.

--- MosaicPlot.m
+++ MosaicPlot.m
@@ -201,7 +201,7 @@ of conditional probabilities from a trie path (suitable to be the second argumen
 Begin["`Private`"]
 
 If[ TrueQ[ Needs["TriesWithFrequencies`"] === $Failed ],
-  Import["https://raw.githubusercontent.com/antononcube/MathematicaForPrediction/master/TriesWithFrequencies.m"];
+  Get["https://raw.githubusercontent.com/antononcube/MathematicaForPrediction/master/TriesWithFrequencies.m"];
 ];
 
 Clear[TrieUniqueRecords]
