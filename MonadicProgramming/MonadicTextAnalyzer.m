5a7f9b087740bde01c35f8f51214e143ce39c0f0
antononcube <antononcube@gmail.com>
2017-11-04 15:16:33 -0400

    Bug fix.

--- MonadicProgramming/MonadicTextAnalyzer.m
+++ MonadicProgramming/MonadicTextAnalyzer.m
@@ -221,7 +221,7 @@ TextAMonComputePOSTags[args___][xs_, context_] :=
         (* Return[
           TextAMon[xs,context] ⟹ TextAMonSentences[] ⟹ TextAMonComputePOSTags[args] ]; *)
         Return[
-          Fold[ TextAMon[xs,context], {TextAMonSentences[], TextAMonComputePOSTags[args]}];
+          Fold[ TextAMon[xs,context], {TextAMonSentences[], TextAMonComputePOSTags[args]}]
         ];
       ];
 
