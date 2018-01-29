63563eaf431c4889d62bacaefb2eb6bd2872e1e7
antononcube <antononcube@gmail.com>
2018-01-29 18:00:37 -0500

    Added implemenation of ParetoLawGGPlot.

--- R/ParetoLawFunctions.R
+++ R/ParetoLawFunctions.R
@@ -55,6 +55,32 @@ ParetoLawPlot <- function( dataVec, main = NULL, xlab="|levels|", ylab="%", xFra
   abline( v=c(xFraction*length(dataVec)), h=c(yFraction), col="red", lty="dotted" )
 }
 
+#' @description Make a plot that demonstrates the adherence to the Pareto law of a numerical vector
+#' with contingency values (counts).
+#' @param dataDF a two column data frame with the first column of a variable name and second column a numerical vector c("Name", "Value")
+#' @param plotTitle a string for the title of the plot
+ParetoLawGGPlot <- function( dataDF, main = NULL, scales = "free_x", ...  ) {
+  pres <- 
+    ddply( dataDF, c("Name"), function(x) {
+      dataVec <- rev( sort(x$Value) )
+      dataVec <- cumsum( dataVec ) / sum( dataVec )
+      data.frame( Name = x$Name[[1]], Index = 1:nrow(x), ParetoFraction = dataVec )
+    })
+
+  qdf <- cbind( pres,
+                p10 = 0.1*nrow(pres), p20 = 0.2*nrow(pres), p30 = 0.3*nrow(pres),  p40 = 0.4*nrow(pres),  p50 = 0.5*nrow(pres) )
+  
+  ggplot(qdf) +
+    geom_line( aes( x = Index, y = ParetoFraction) ) +
+    geom_vline( aes( xintercept = p10), linetype = 3 ) +
+    geom_vline( aes( xintercept = p20), linetype = 3 ) +
+    geom_vline( aes( xintercept = p30), linetype = 3 ) +
+    geom_vline( aes( xintercept = p40), linetype = 3 ) +
+    geom_vline( aes( xintercept = p50), linetype = 3 ) +
+    facet_wrap( ~ Name, scales = scales ) +
+    theme( ... )
+}
+
 #' @description Sorts the tally of given categorical data descendingly and computes the list of the cumulative sums.
 #' @param dataVec a vector with categorical entries
 #' @param weightVec a vector of weights for the values of dataVec; see the argument wt_var of plyr::count
