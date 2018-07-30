ac97876e1941cbae5b21a9a27bcb2b013b2018af
Anton Antonov <antononcube@gmail.com>
2018-07-30 04:57:41 -0400

    Bug fix.

--- MonadicProgramming/MonadicQuantileRegression.m
+++ MonadicProgramming/MonadicQuantileRegression.m
@@ -671,7 +671,7 @@ QRMonErrors[][xs_, context_] :=
           Association @
               KeyValueMap[
                 Function[{k, f},
-                  k -> Map[ Function[{p}, {p[[1]], (f[p[[1]]] - p[[2]])/p[[2]]}], context["data"] ]
+                  k -> Map[ Function[{p}, {p[[1]], (f[p[[1]]] - p[[2]])/ If[ p[[2]] == 0, 1, p[[2]] ]}], context["data"] ]
                 ],
                 context["regressionFunctions"]
               ];
@@ -707,7 +707,7 @@ QRMonErrorPlots[opts:OptionsPattern[]][xs_, context_] :=
             Function[{k, f},
               k ->
                   listPlotFunc[
-                    Map[Function[{p}, {p[[1]], (f[p[[1]]] - p[[2]])/p[[2]]}], context["data"] ],
+                    Map[Function[{p}, {p[[1]], (f[p[[1]]] - p[[2]]) / If[ p[[2]] == 0, 1, p[[2]] ] }], context["data"] ],
                     listPlotOpts,
                     Joined->False, PlotRange -> All, Filling -> Axis, Frame -> True, ImageSize -> Medium, PlotTheme -> "Scientific"]
             ],
