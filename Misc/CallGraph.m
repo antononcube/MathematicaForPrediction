be5614c08d0dc6f609c5f0f421cdf7ad447f0db7
Anton Antonov <antononcube@gmail.com>
2018-12-31 01:45:00 -0500

    Added license.

--- Misc/CallGraph.m
+++ Misc/CallGraph.m
@@ -1,6 +1,35 @@
-(* Mathematica Package *)
-(* Created by Mathematica Plugin for IntelliJ IDEA, see http://wlplugin.halirutan.de/ *)
+(*
+    Call graph for context functions Mathematica package
+    Copyright (C) 2018  Anton Antonov
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Written by Anton Antonov,
+    antononcube @ gmail . com,
+    Windermere, Florida, USA.
+*)
+
+(*
+    Mathematica is (C) Copyright 1988-2018 Wolfram Research, Inc.
+
+    Protected by copyright law and international treaties.
+
+    Unauthorized reproduction or distribution subject to severe civil
+    and criminal penalties.
+
+    Mathematica is a registered trademark of Wolfram Research, Inc.
+*)
 (* :Title: CallGraph *)
 (* :Context: CallGraph` *)
 (* :Author: Anton Antonov *)
@@ -9,11 +38,10 @@
 (* :Package Version: 0.6 *)
 (* :Mathematica Version: 11.3 *)
 (* :Copyright: (c) 2018 Anton Antonov *)
-(* :Keywords: *)
+(* :Keywords: call graph, dependencies, down values, sub values *)
 (* :Discussion: *)
 
 BeginPackage["CallGraph`"];
-(* Exported symbols added here with SymbolName::usage *)
 
 CallGraph::usage = "CallGraph[contexts:{_String..}, opts___] makes a call graph for the functions of \
 specified (package) contexts."
