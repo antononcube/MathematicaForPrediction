9811dbb9c760d39cc25b7900ae3d0afb4a0eab61
antononcube <antononcube@posteo.net>
2022-09-16 12:23:22 -0400

    docs:Better year.

--- Misc/NeuralNetworkGraph.m
+++ Misc/NeuralNetworkGraph.m
@@ -1,6 +1,6 @@
 (*
     Neural network graph Mathematica package
-    Copyright (C) 2018  Anton Antonov
+    Copyright (C) 2018-2022  Anton Antonov
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
