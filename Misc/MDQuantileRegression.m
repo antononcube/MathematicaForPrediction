4f81ed1b87204140ca47e66aa2d298318c1e3c03
antononcube <antononcube@posteo.net>
2023-01-07 21:11:04 -0500

    refactor:Better basis making function name and signature. feat:Association of "function center" to function.

--- Misc/MDQuantileRegression.m
+++ Misc/MDQuantileRegression.m
@@ -32,19 +32,19 @@
 *)
 (* :Title: MDQuantileRegression *)
 (* :Context: MDQuantileRegression` *)
-(* :Author: antonov *)
+(* :Author: Anton Antonov *)
 (* :Date: 2023-01-07 *)
 
 (* :Package Version: 0.1 *)
 (* :Mathematica Version: 13.1 *)
-(* :Copyright: (c) 2023 antonov *)
+(* :Copyright: (c) 2023 Anton Antonov *)
 (* :Keywords: *)
 (* :Discussion: *)
 
 BeginPackage["MDQuantileRegression`"];
 (* Exported symbols added here with SymbolName::usage *)
 
-QRBasisFunctions::usage = "QRBasisFunctions[data_, knots_, order_]";
+BSplineFunctionsBasis::usage = "BSplineFunctionsBasis[data_, knots_, order_]";
 
 MDQuantileRegression::usage = "MDQuantileRegression[data_, knots_, order_, probs, opts___]";
 
@@ -56,27 +56,31 @@ End[]; (* `Private` *)
 (* Make basis functions                                     *)
 (************************************************************)
 
-Clear[QRBasisFunctions];
+Clear[BSplineFunctionsBasis];
 
-QRBasisFunctions::nargs = "The first argument is expected to be a matrix; \
-the second argument is expected to be a positive integer or a list of positive integers; \
-the third argument is expected to be an integer.";
+BSplineFunctionsBasis::nargs = "The first argument is expected to be a matrix; \
+the second argument is expected to be a positive integer or a list of positive integers;
+the third argument is expected to be one List of Association.";
 
-QRBasisFunctions[data_?MatrixQ, nPieces_Integer, order_Integer, opts : OptionsPattern[]] :=
-    QRBasisFunctions[data, Rescale[Range[0, 1, 1 / nPieces], {0, 1}, {Min[data[[All, 1]]], Max[data[[All, 1]]]}], order, opts] /; Dimensions[data][[2]] == 2;
+BSplineFunctionsBasis::nfmt = "The third argument is expected to be one of List or Association. (Continuing with List.)";
 
-QRBasisFunctions[data_?MatrixQ, knots_List, order_Integer, opts : OptionsPattern[]] :=
+Options[BSplineFunctionsBasis] = Options[BSplineFunction];
+
+BSplineFunctionsBasis[data_?MatrixQ, nPieces_Integer, format_?AtomQ, opts : OptionsPattern[]] :=
+    BSplineFunctionsBasis[data, Rescale[Range[0, 1, 1 / nPieces], {0, 1}, {Min[data[[All, 1]]], Max[data[[All, 1]]]}], format, opts] /; Dimensions[data][[2]] == 2;
+
+BSplineFunctionsBasis[data_?MatrixQ, knots_List, format_?AtomQ, opts : OptionsPattern[]] :=
     Block[{},
-      Echo["Not implemented yet.", "QRBasisFunctions::"];
+      Echo["Not implemented yet.", "BSplineFunctionsBasis::"];
       $Failed
     ] /; Dimensions[data][[2]] == 2;
 
-QRBasisFunctions[data_?MatrixQ, nKnots_Integer, order_Integer, opts : OptionsPattern[]] :=
-    QRBasisFunctions[data, Table[nKnots, Dimensions[data][[2]] - 1], order, opts] /; Dimensions[data][[2]] > 2;
+BSplineFunctionsBasis[data_?MatrixQ, nKnots_Integer, format_?AtomQ, opts : OptionsPattern[]] :=
+    BSplineFunctionsBasis[data, Table[nKnots, Dimensions[data][[2]] - 1], format, opts] /; Dimensions[data][[2]] > 2;
 
-QRBasisFunctions[data_?MatrixQ, nKnotsArg : {_Integer..}, order_Integer, opts : OptionsPattern[]] :=
-    Block[{nKnots = nKnotsArg, m, n, lsMinMaxes, cpts0, lsInds,
-      lsSlotArgs, lsSlotArgsRescaled, lsBasis, lsBasisRescaled},
+BSplineFunctionsBasis[data_?MatrixQ, nKnotsArg : {_Integer..}, format_?AtomQ, opts : OptionsPattern[]] :=
+    Block[{nKnots = nKnotsArg, m, n, lsMinMaxes, cpts0, lsIndexes,
+      lsSlotArgs, lsSlotArgsRescaled, lsBasis, lsKeys},
 
       (* Get dimensions *)
       {m, n} = Dimensions[data];
@@ -85,30 +89,42 @@ QRBasisFunctions[data_?MatrixQ, nKnotsArg : {_Integer..}, order_Integer, opts : 
       lsMinMaxes = Table[ MinMax[data[[All, i]]], {i, 1, n - 1}];
 
       (* Process knots argument *)
-      If[ Length[nKnots] < n - 1, nKnots = Flatten[Table[nKnots, n - 1]][[ 1 ;; n - 1 ]]];
+      If[ Length[nKnots] < n - 1,
+        nKnots = Flatten[Table[nKnots, n - 1]][[ 1 ;; n - 1 ]]
+      ];
 
       (* Prepare control points *)
       cpts0 = ConstantArray[{0}, nKnots];
 
       (* Cartesian product of indexes *)
-      lsInds = Flatten[Outer[List, Sequence @@ Map[Range, nKnots]], Length[nKnots] - 1];
+      lsIndexes = Flatten[Outer[List, Sequence @@ Map[Range, nKnots]], Length[nKnots] - 1];
 
       (* Make basis functions slots *)
       lsSlotArgs = Table[Slot[i], {i, Length[lsMinMaxes]}];
       lsSlotArgsRescaled = Table[Rescale[Slot[i], lsMinMaxes[[i]], {0, 1}], {i, Length[lsMinMaxes]}];
 
       (* Make basis functions control points *)
-      lsBasis = Map[ReplacePart[cpts0, # -> {1}]&, lsInds];
+      lsBasis = Map[ReplacePart[cpts0, # -> {1}]&, lsIndexes];
 
       (* Make basis functions *)
       lsBasis = With[{f = BSplineFunction[#, Sequence @@ FilterRules[{opts}, Options[BSplineFunction]]], args = lsSlotArgsRescaled}, f @@ args&]& /@ lsBasis;
 
       (* Result *)
       (* <| "Unitary" -> lsBasis, "Rescaled" -> lsBasisRescaled |> *)
-      lsBasis
+      Which[
+        MemberQ[{List, "List"}, format], lsBasis,
+
+        MemberQ[{Association, "Association"}, format],
+        lsKeys = Transpose @ MapThread[Rescale[#1, {1, #2}, #3]&, {Transpose[lsIndexes], nKnots, lsMinMaxes}];
+        AssociationThread[lsKeys, lsBasis],
+
+        True,
+        Message[BSplineFunctionsBasis::nfmt];
+        lsBasis
+      ]
     ] /; Dimensions[data][[2]] > 2;
 
-QRBasisFunctions[___] := $Failed;
+BSplineFunctionsBasis[___] := $Failed;
 
 (************************************************************)
 (* MDQuantileRegression                                     *)
@@ -121,9 +137,9 @@ The expression n - i - 2 should be non-negative, where n is the number of knots 
 
 MDQuantileRegression::"zerob" = "The specified knots `1` and interpolation order `2` produced a list of zeroes instead of a list of B-Spline basis functions.";
 
-MDQuantileRegression[dataArg_?MatrixQ, knotsSpec : ( _Integer | {_Integer..} ), order_Integer, probs : {_?NumberQ ..}, opts : OptionsPattern[]] :=
+MDQuantileRegression[dataArg_?MatrixQ, knotsSpec : ( _Integer | {_Integer..} ), splineDegree_Integer, probs : {_?NumberQ ..}, opts : OptionsPattern[]] :=
     Block[{data = dataArg, yMedian = 0, yFactor = 1, yShift = 0, n = Dimensions[dataArg][[1]],
-      aRes, pFuncs, pFuncsRescaled, c, t, qrSolutions, dataRescaled, mat},
+      pFuncs, c, t, qrSolutions, mat},
 
       (* Transform data *)
       If[Min[data[[All, -1]]] < 0,
@@ -135,16 +151,16 @@ MDQuantileRegression[dataArg_?MatrixQ, knotsSpec : ( _Integer | {_Integer..} ), 
       ];
 
       (* B-spline basis functions *)
-      pFuncs = QRBasisFunctions[data, knotsSpec, order];
-(*      pFuncs = aRes["Unitary"];*)
-(*      pFuncsRescaled = aRes["Rescaled"];*)
+      pFuncs = BSplineFunctionsBasis[data, knotsSpec, List, "SplineDegree" -> splineDegree];
+      (*      pFuncs = aRes["Unitary"];*)
+      (*      pFuncsRescaled = aRes["Rescaled"];*)
 
       (* Create the conditions matrix *)
-(*      dataRescaled = Transpose[ Clip[Rescale[#, MinMax[#], {0, 1}], {0, 1}] & /@ Transpose[ data[[All, 1 ;; -2]] ]];*)
+      (*      dataRescaled = Transpose[ Clip[Rescale[#, MinMax[#], {0, 1}], {0, 1}] & /@ Transpose[ data[[All, 1 ;; -2]] ]];*)
       mat = Transpose @ Map[ Flatten[# @@@ data[[All, 1 ;; -2]]]&, pFuncs];
-(*      Print[Dimensions[mat]];*)
-(*      Print[MatrixQ[mat, NumericQ]];*)
-(*      Print[mat];*)
+      (*      Print[Dimensions[mat]];*)
+      (*      Print[MatrixQ[mat, NumericQ]];*)
+      (*      Print[mat];*)
       mat = MapThread[Join, {mat, IdentityMatrix[n], -IdentityMatrix[n]}];
 
       mat = SparseArray[mat];
